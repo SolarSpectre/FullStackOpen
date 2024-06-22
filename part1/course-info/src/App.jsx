28d027fc36f2389e84e4bc34066e85e371a8fa3d
SolarSpectre <josephcaza32@gmail.com>
2024-06-21 21:03:03 -0500

    Step 1

--- part1/course-info/src/App.jsx
+++ part1/course-info/src/App.jsx
@@ -1,3 +1,26 @@
+const Header = (course) => {
+  return(
+  <h1>{course.name}</h1>
+)}
+const Content = (Content) => {
+  return(
+  <>
+    <p>
+      {Content.part1} {Content.exercises1}
+    </p>
+    <p>
+      {Content.part2} {Content.exercises2}
+    </p>
+    <p>
+      {Content.part3} {Content.exercises3}
+    </p>
+  </>
+)}
+const Total = (num) => {
+  return(
+  <p>Number of exercises {num.sum}</p>
+)}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +32,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total sum={exercises1+exercises2+exercises3}/>
     </div>
   )
 }
