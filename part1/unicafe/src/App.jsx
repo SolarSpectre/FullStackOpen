457c9e8b82ad3101e94679bafb7c693d0a04a371
SolarSpectre <josephcaza32@gmail.com>
2024-06-25 20:53:03 -0500

    step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,25 +1,27 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Button = ({name,counter,setValue}) => {
-  const handleClick=()=>{
-    setValue(counter+1)
-  }
+const Button = ({ name, counter, setValue }) => {
+  const handleClick = () => {
+    setValue(counter + 1);
+  };
   return (
     <button onClick={handleClick}>{name}</button>
-  )
-}
+  );
+};
 
-const Statistics = ({name, counter}) =>{
-  return(
+const Statistics = ({ name, counter }) => {
+  return (
     <p>{name} {counter}</p>
-  )
-}
+  );
+};
 
 function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good - bad) / all : 0;
+  const positive = all > 0 ? (good / all) * 100 : 0;
   return (
     <div>
       <h1>Give feedback</h1>
@@ -27,11 +29,16 @@ function App() {
       <Button name="neutral" counter={neutral} setValue={setNeutral} />
       <Button name="bad" counter={bad} setValue={setBad} />
       <h1>Statistics</h1>
-      <Statistics name="good" counter={good}/>
-      <Statistics name="neutral" counter={neutral}/>
-      <Statistics name="bad" counter={bad}/>
+      <Statistics name="good" counter={good} />
+      <Statistics name="neutral" counter={neutral} />
+      <Statistics name="bad" counter={bad} />
+      
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
