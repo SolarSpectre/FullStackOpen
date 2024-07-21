248a52fa969b92b2933b2db066a0dec7cf6ceb95
SolarSpectre <josephcaza32@gmail.com>
2024-07-21 14:42:45 -0500

    2.11

--- part2/phone-agency/src/App.jsx
+++ part2/phone-agency/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
-import Filter from './components/Filter';
-import PersonForm from './components/PersonForm';
-import Persons from './components/Persons';
+import { useState, useEffect } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]); 
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -31,21 +32,21 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
-      id: persons.length + 1 // ensuring unique id
+      id: persons.length + 1, // ensuring unique id
     };
 
-    if (persons.some(person => person.name === newName)) {
+    if (persons.some((person) => person.name === newName)) {
       alert(`${personObject.name} is already added to phonebook`);
-    } else if (persons.some(person => person.number === newNumber)) {
+    } else if (persons.some((person) => person.number === newNumber)) {
       alert(`${personObject.number} is already added to phonebook`);
     } else {
       setPersons(persons.concat(personObject));
-      setNewName('');
-      setNewNumber('');
+      setNewName("");
+      setNewNumber("");
     }
   };
 
-  const personsToShow = persons.filter(person =>
+  const personsToShow = persons.filter((person) =>
     person.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -67,4 +68,3 @@ const App = () => {
 };
 
 export default App;
-
