dea5c5f8307d664194b0ff8ab6c19392102cca07
SolarSpectre <josephcaza32@gmail.com>
2024-09-14 10:47:47 -0500

    3.4

---
+++ part3/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express()
+const now = new Date();
+
+const PORT = 3001
+
+let notes = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(notes);
+})
+
+app.get('/info',(request,response)=>{
+  response.send(`<p>Phonebook has info for ${notes.length} people</p><p>${now.toString()}</p>`)
+})
+
+app.get('/api/persons/:id', (request,response) => {
+  const id = Number(request.params.id) 
+  const note = notes.find(note => note.id === id)
+  if(note){
+    response.json(note)
+  }else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request,response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
