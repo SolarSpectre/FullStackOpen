42dd8f38e9ab2ecaba8af4752bcb22a038a0fa7f
SolarSpectre <josephcaza32@gmail.com>
2024-10-11 20:03:13 -0500

    4.8

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,15 +1,30 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
+const helper = require("./test_helper");
 const api = supertest(app);
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  for (const blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
 
 test("notes are returned as json", async () => {
+  console.log("entered test");
   await api
-    .get("/api/notes")
+    .get("/api/blogs")
     .expect(200)
-    .expect("Content-Type", /application\/json/);
+    .expect("content-type", /application\/json/);
+});
+test("all blogs are returned", async () => {
+  const response = await api.get("/api/blogs");
+  assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
 after(async () => {
