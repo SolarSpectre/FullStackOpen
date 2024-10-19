6da729398c595a6bdda63703219e67dbd845e289
SolarSpectre <josephcaza32@gmail.com>
2024-10-19 09:34:54 -0500

    4.14

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -15,7 +15,7 @@ beforeEach(async () => {
   }
 });
 
-test("notes are returned as json", async () => {
+test("blogs are returned as json", async () => {
   console.log("entered test");
   await api
     .get("/api/blogs")
@@ -32,7 +32,45 @@ test("id property exists", async () => {
     assert.strictEqual(typeof blog.id, "string");
   });
 });
+test("delete a blog correctly", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
 
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+  const ids = blogsAtEnd.map((r) => r.id);
+  assert(!ids.includes(blogToDelete.id));
+});
+test("update a blog correctly", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToUpdate = blogsAtStart[1];
+
+  const updatedBlogData = {
+    title: blogToUpdate.title,
+    author: blogToUpdate.author,
+    url: blogToUpdate.url,
+    likes: 200,
+  };
+
+  const response = await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedBlogData)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+
+  const updatedBlogFromResponse = response.body;
+
+  assert.strictEqual(updatedBlogFromResponse.likes, updatedBlogData.likes);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  const blogAfterUpdate = blogsAtEnd.find((b) => b.id === blogToUpdate.id);
+
+  assert.strictEqual(blogAfterUpdate.likes, updatedBlogData.likes);
+});
 after(async () => {
   await mongoose.connection.close();
 });
