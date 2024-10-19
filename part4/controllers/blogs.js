f65432885501f9aedb7c6bb3a3c5ea98cd50b51b
SolarSpectre <josephcaza32@gmail.com>
2024-10-19 12:19:46 -0500

    user

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,14 +1,26 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog.js");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const notes = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
+  const user = await User.findById(body.userId);
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  });
   const savedBlog = await blog.save();
+
+  user.notes = blog.notes.concat(savedBlog._id);
+  await user.save();
   response.status(201).json(savedBlog);
 });
 
