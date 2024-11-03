d66f2b23f42240f2d00391e48fb8cf19b5fc7b9b
SolarSpectre <josephcaza32@gmail.com>
2024-11-02 19:41:26 -0500

    5.2

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,12 +2,23 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
   const handleLogin = async (event) => {
     event.preventDefault();
     try {
@@ -15,6 +26,7 @@ const App = () => {
         username,
         password,
       });
+      window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
       setUsername("");
@@ -28,41 +40,70 @@ const App = () => {
       }, 5000);
     }
   };
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-        {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
-        <form onSubmit={handleLogin}>
-          <div>
-            username
-            <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password
-            <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-      </div>
-    );
-  }
+
+  const handleLogout = () => {
+    setUser(null);
+    blogService.setToken(null);
+    window.localStorage.removeItem("loggedBlogAppUser");
+    setBlogs([]);
+  };
+
+  const fetchBlogs = async () => {
+    try {
+      const blogs = await blogService.getAll();
+      setBlogs(blogs);
+    } catch (error) {
+      setErrorMessage("Error fetching blogs");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchBlogs();
+    }
+  }, [user]);
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {user === null ? (
+        <div>
+          <h2>Log in to application</h2>
+          {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
+          <form onSubmit={handleLogin}>
+            <div>
+              username
+              <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div>
+              password
+              <input
+                type="password"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
+        </div>
+      ) : (
+        <div>
+          <h2>blogs</h2>
+          <p>{user.username} is logged in</p>
+          <button onClick={handleLogout}>Logout</button>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
